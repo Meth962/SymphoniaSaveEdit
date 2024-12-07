34ae295ce2ef3c74d19543d0818e82cb8737710a
Geoff Wakefield <g.wakefield@hotmail.com>
2024-12-07 11:39:39 -0600

    Add max collector's button. Add setttings for file type/path.

---
+++ SymphoniaSaveEdit/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymphoniaSaveEdit
+{
+    internal class Settings
+    {
+        public string LastSaveLocation { get; set; }
+        public int LastSaveType { get; set; }
+    }
+}
